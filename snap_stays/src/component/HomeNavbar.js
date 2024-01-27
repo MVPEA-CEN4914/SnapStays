5bfe4fe2d5f46def1cb885b200286fade494a4f8
victoriadealba <victoriadealba@ufl.edu>
2024-01-27 17:13:01 -0500

    Created updated nav bar with route to login page and landing page

--- snap_stays/src/component/HomeNavbar.js
+++ snap_stays/src/component/HomeNavbar.js
@@ -1,27 +1,29 @@
 import React from 'react'
-
-const settings = ['Signup'];
+import AppBar from '@mui/material/AppBar';
+import Stack from '@mui/material/Stack'
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import {Router, Link as RouterLink} from "react-router-dom";
+import BlackLogo from "../images/BlackLogo.png"
 
 function HomeNavbar() {
-    const [anchorNav, setAnchorNav] = React.useState(null);
-    const [anchorUser, setAnchorUser] = React.useState(null);
-    const handleOpenNavMenu = (event) => {
-        setAnchorNav(event.currentTarget);
-      };
-      const handleOpenUserMenu = (event) => {
-        setAnchorUser(event.currentTarget);
-      };
-    
-      const handleCloseNavMenu = () => {
-        setAnchorNav(null);
-      };
-    
-      const handleCloseUserMenu = () => {
-        setAnchorUser(null);
-    };
-    
   return (
-    <div>HomeNavbar</div>
+    <AppBar position="static" sx={{bgcolor: '#E6E6DD'}}>
+      <Toolbar>
+       <IconButton edge = 'start' component = {RouterLink} to = "/">
+          <img src = {BlackLogo} alt = "BlackLogo"style={{ width: '50px', height: 'auto' }} />
+          </IconButton>
+          <Stack direction = 'row' spacing={2} sx={{ ml: 'auto' }}>
+          <Button variant = 'contained' component= {RouterLink} to= "/login">Login</Button>
+          </Stack>
+        
+      </Toolbar>
+    </AppBar>
+
   )
 }
 
