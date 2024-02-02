14aec569df4e2ea7028b317c5bef434da1bb5345
Alyssa Foglia <alyssafog@gmail.com>
2024-02-02 10:18:59 -0500

    landing page almost there

--- snap_stays/src/pages/Landing.js
+++ snap_stays/src/pages/Landing.js
@@ -92,8 +92,8 @@ function Landing() {
         </div>
         <div
           style={{
-            width: "75%",
-            height: "100%",
+            width: "75vw",
+            height: "100vw",
             left: "25%",
             top: "450px",
             position: "absolute",
@@ -105,13 +105,13 @@ function Landing() {
             component={RouterLink}
             to="/ListStay"
             sx={{
-              width: "225px",
-              height: "75px",
+              width: "17vw",
+              height: "10vh",
               position: "absolute",
               left: "0px",
               top: "0px",
               textAlign: "center",
-              fontSize: "24px",
+              fontSize: "2vw",
               fontFamily: "Josefin Sans",
               fontWeight: "300",
               wordWrap: "break-word",
@@ -124,9 +124,9 @@ function Landing() {
         </div>
         <img
           style={{
-            width: "45%",
-            left: "50%",
-            top: "50px",
+            width: "45vw",
+            left: "50vw",
+            top: "5vh",
             position: "absolute",
           }}
           src={houseImage}
@@ -135,17 +135,17 @@ function Landing() {
       </div>
       <div
         style={{
-          width: "100%",
-          height: "100%",
+          width: "100vw",
+          height: "3vh",
           left: "0%",
-          top: "700px",
+          top: "100vh",
           position: "absolute",
         }}
       >
-        <div
+        <div //tan background
           style={{
-            width: "100%",
-            height: "100vh",
+            width: "100vw",
+            height: "200vh",
             top: "13.4vh",
             position: "absolute",
             background: "#AF8C53",
@@ -153,18 +153,30 @@ function Landing() {
         ></div>
         <div
           style={{
-            width: "90%",
-            height: "100%",
-            left: "3%",
+            width: "90vw",
+            height: "3vh",
+            left: "3vw",
             top: "0px",
             position: "absolute",
           }}
         >
-          <div
+          <div //bottom of 3 bars
             style={{
-              width: "106%",
-              height: "190%",
-              top: "97px",
+              width: "96vw",
+              height: "5vh",
+              top: "13vh",
+              position: "absolute",
+              background: "#AF8C53",
+              borderRadius: "10px",
+              border: "3px #2B2B2B solid",
+            }}
+          ></div>
+          <div //middle of 3 bars
+            style={{
+              width: "93vw",
+              height: "5vh",
+              left: "2vw",
+              top: "9vh",
               position: "absolute",
               background: "#AF8C53",
               borderRadius: "10px",
@@ -172,35 +184,23 @@ function Landing() {
             }}
           ></div>
           <div
-            style={{
-              width: "103%",
-              height: "190%",
-              left: "20px",
-              top: "60px",
+            style={{ //top of 3 bars
+              width: "89vw",
+              height: "5vh",
+              left: "4vw",
+              top: "5vh",
               position: "absolute",
               background: "#AF8C53",
               borderRadius: "10px",
               border: "3px #2B2B2B solid",
             }}
           ></div>
-          <div
+          <div //left dark brown bar
             style={{
-              width: "100%",
-              height: "190%",
-              left: "40px",
-              top: "23px",
-              position: "absolute",
-              background: "#AF8C53",
-              borderRadius: "10px",
-              border: "3px #2B2B2B solid",
-            }}
-          ></div>
-          <div
-            style={{
-              width: "15%",
-              height: "150%",
+              width: "14vw",
+              height: "5vh",
               left: "0px",
-              top: "181.31px",
+              top: "24vh",
               position: "absolute",
               transform: "rotate(-65deg)",
               transformOrigin: "0 0",
@@ -209,11 +209,11 @@ function Landing() {
               border: "3px #2B2B2B solid",
             }}
           ></div>
-          <div
+          <div //right dark brown bar
             style={{
-              width: "15%",
-              height: "150%",
-              left: "1300px",
+              width: "14vw",
+              height: "5vh",
+              left: "91vw",
               top: "0px",
               position: "absolute",
               transform: "rotate(65deg)",
@@ -224,32 +224,32 @@ function Landing() {
             }}
           ></div>
         </div>
-        <div
+        <div //grey box on second page
           style={{
-            width: "80%",
-            height: "100%",
-            left: "125px",
-            top: "277px",
+            width: "80vw",
+            height: "2.5vh",
+            left: "8vw",
+            top: "35vh",
             position: "absolute",
           }}
         >
           <div
             style={{
-              width: "86%",
-              height: "0%",
-              left: "28px",
-              top: "0px",
+              width: "86vw",
+              height: "0vh",
+              left: "28vw",
+              top: "0vh",
               position: "absolute",
               background: "black",
               borderRadius: "30px",
             }}
           ></div>
-          <div
+          <div //grey box on second page
             style={{
-              width: "90%",
-              height: "2900%",
-              left: "50px",
-              top: "0px",
+              width: "73vw",
+              height: "75vh",
+              left: "5vw",
+              top: "0vh",
               position: "absolute",
               background: "#2B2B2B",
               borderRadius: "30px",
@@ -257,14 +257,14 @@ function Landing() {
             }}
           ></div>
         </div>
-        <div
+        <div //"welcome in enjoy your stay" text
           style={{
-            width: "666px",
-            left: "360px",
-            top: "318px",
+            width: "70vw",
+            left: "15vw",
+            top: "40vh",
             position: "absolute",
             color: "#E6E6DD",
-            fontSize: "48px",
+            fontSize: "4vw",
             fontFamily: "Josefin Sans",
             fontStyle: "italic",
             fontWeight: "700",
@@ -273,14 +273,14 @@ function Landing() {
         >
           Welcome In, Enjoy Your Stay!
         </div>
-        <div
+        <div //paragraph text
           style={{
-            width: "65%",
-            left: "15%",
-            top: "382px",
+            width: "65vw",
+            left: "17vw",
+            top: "50vh",
             position: "absolute",
             color: "#E6E6DD",
-            fontSize: "40px",
+            fontSize: "3vw",
             fontFamily: "Josefin Sans",
             fontWeight: "400",
             wordWrap: "break-word",
@@ -296,7 +296,7 @@ function Landing() {
       <div
         style={{
           width: "100%",
-          height: "200%",
+          height: "220%",
           left: "0px",
           top: "1691px",
           position: "absolute",
@@ -313,10 +313,10 @@ function Landing() {
         ></div>
         <img //dog image
           style={{
-            width: "555px",
-            height: "559px",
-            left: "78px",
-            top: "229px",
+            width: "40vw",
+            height: "70vh",
+            left: "5vw",
+            top: "30vh",
             position: "absolute",
           }}
           src={dogImage}
@@ -324,13 +324,13 @@ function Landing() {
         />
         <div
           style={{
-            width: "80%",
-            left: "%",
-            top: "99px",
+            width: "80vw",
+            left: "10vw",
+            top: "10vh",
             position: "absolute",
             textAlign: "right",
             color: "#E6E6DD",
-            fontSize: "65px",
+            fontSize: "5vw",
             fontFamily: "Josefin Sans",
             fontWeight: "700",
             wordWrap: "break-word",
@@ -340,13 +340,13 @@ function Landing() {
         </div>
         <div
           style={{
-            width: "508px",
-            left: "685px",
-            top: "300px",
+            width: "40vw",
+            left: "50vw",
+            top: "40vh",
             position: "absolute",
             textAlign: "right",
             color: "#E6E6DD",
-            fontSize: "65px",
+            fontSize: "4vw",
             fontFamily: "Josefin Sans",
             fontWeight: "400",
             wordWrap: "break-word",
