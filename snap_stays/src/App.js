eab8831b5a178fa0119833a2d16e8c4e91480120
MarianaTorresTorres <mari.torret@gmail.com>
2024-03-04 19:07:13 -0500

    front end forgot password styling done

--- snap_stays/src/App.js
+++ snap_stays/src/App.js
@@ -1,54 +1,56 @@
-import './App.css';
+import "./App.css";
 import ReactDom from "react-dom/client";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Layout from './pages/Layout';
-import FindStay from './pages/FindStay';
-import ListStay from './pages/ListStay';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Layout from "./pages/Layout";
+import FindStay from "./pages/FindStay";
+import ListStay from "./pages/ListStay";
 import Verify from "./pages/Verify";
-import {ThemeProvider, createTheme} from '@mui/material/styles'
-import { CssBaseline } from '@mui/material';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import ForgotPassword from "./pages/ForgotPassword";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const theme = createTheme({
-  palette:{
+  palette: {
     background: {
-      default:'#E6E6DD' // Beige
+      default: "#E6E6DD", // Beige
     },
     primary: {
-      light: '#E6E6DD',
-      main: '#AF8C53', // Mustard
+      light: "#CDB285", // Light Mustard (for hover effect)
+      main: "#AF8C53", // Mustard
     },
-    secondary:{
-      main:'#2B2B2B' // Black
-    }
-    
-  }
+    secondary: {
+      light: "#2B2B2B", // Gray
+      main: "#2B2B2B", // Black
+    },
+  },
 });
 
 function App() {
   return (
     <ApolloProvider>
-    <div className="App">
-      <ThemeProvider theme = {theme}>
-        <CssBaseline />
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element={<Layout />}>
-        <Route index element={<Landing />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="FindStay" element = {<FindStay/>} />
-        <Route path="ListStay" element = {<ListStay/>} />
-        <Route exact path="/verify/:id" element={<Verify/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </ThemeProvider>
-    </div>
-   </ApolloProvider>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<Landing />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="find-stay" element={<FindStay />} />
+                <Route path="list-stay" element={<ListStay />} />
+                <Route exact path="/verify/:id" element={<Verify />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </ThemeProvider>
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ snap_stays/src/pages/ForgotPassword.js
@@ -0,0 +1,197 @@
+import * as React from "react";
+import { useNavigate } from "react-router-dom";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import PasswordIcon from "@mui/icons-material/Password";
+import Typography from "@mui/material/Typography";
+import Houses from "../images/Houses.png";
+import axios from "axios";
+import { useForm } from "../hooks/hooks";
+import { gql } from "graphql-tag";
+import { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { useTheme } from "@mui/material/styles";
+
+/*TO DO:
+- Functionality
+- Make the 5rem padding less or none for smaller screens
+*/
+
+function ForgotPassword() {
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const [errors, setErrors] = useState({});
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChange = (event) => {
+    const { name, value } = event.target; // Destructure name and value from event.target
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value, // Update the corresponding state property using computed property syntax
+    }));
+  };
+  const [loginUser, { loading }] = useMutation(LOGIN, {
+    update(_, result) {
+      console.log("Mutation result:", result);
+      navigate("/");
+    },
+    onError(err) {
+      console.log("Mutation error:", err);
+      if (err.graphQLErrors && err.graphQLErrors.length > 0) {
+        const extensionErrors = err.graphQLErrors[0]?.extensions?.errors;
+        if (extensionErrors) {
+          // Populate errors state with extension error messages
+          setErrors(extensionErrors);
+        } else {
+          // If extension errors not found, set a generic error message
+          setErrors({ general: "An error occurred" });
+        }
+      } else {
+        // If there are no GraphQL errors, set a generic error message
+        setErrors({ general: "An error occurred" });
+      }
+    },
+    variables: values,
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form values:", values);
+    loginUser();
+  };
+  return (
+    <Grid
+      container
+      component="main"
+      sx={{
+        height: "90vh",
+        paddingX: "5rem",
+        backgroundColor: theme.palette.background.default,
+      }}
+    >
+      <Grid
+        item
+        xs={false}
+        sm={6}
+        md={6}
+        sx={{
+          backgroundImage: `url(${Houses})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      />
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={6}
+        component={Paper}
+        elevation={6}
+        square
+        sx={{ backgroundColor: theme.palette.background.default, boxShadow: 0 }}
+      >
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
+            <PasswordIcon />
+          </Avatar>
+          <Typography
+            variant="h4"
+            sx={{ paddingY: "1rem", fontWeight: "bold" }}
+          >
+            Forgot Password
+          </Typography>
+          <Typography variant="body1" sx={{ textAlign: "center" }}>
+            Input your email to receive a recovery email.
+          </Typography>
+          <Typography variant="body1" sx={{ textAlign: "center" }}>
+            You will be redirected to the login page after.
+          </Typography>
+          <Box
+            component="form"
+            noValidate
+            textAlign='center'
+            onSubmit={handleSubmit}
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              value={values.email}
+              error={errors.email ? true : false}
+              onChange={onChange}
+              helperText={errors.email ? errors.email : ""}
+              InputProps={{
+                style: {
+                  borderRadius: "30px",
+                }
+              }}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                paddingY: "0.5rem",
+                paddingX: "1rem",
+                borderRadius: "30px",
+                bgcolor: theme.palette.primary.main,
+                "&:hover": {
+                  bgcolor: theme.palette.primary.light,
+                },
+              }}
+            >
+              Send Recovery Email
+            </Button>
+            {Object.keys(errors).length > 0 && (
+              <div className="ui error message">
+                <ul className="list" sx={{ color: "red" }}>
+                  {Object.entries(errors).map(
+                    ([fieldName, errorMessage], index) => (
+                      <li key={index}>{errorMessage}</li>
+                    )
+                  )}
+                </ul>
+              </div>
+            )}
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+const LOGIN = gql`
+  mutation loginUser($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      token
+      fullName
+    }
+  }
+`;
+
+export default ForgotPassword;
