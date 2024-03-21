5affb88252f04a77afdd7739af7c66adc7097739
victoriadealba <victoriadealba@ufl.edu>
2024-03-21 17:24:21 -0400

    was able to query the user

--- snap_stays/src/component/HomeNavbar.js
+++ snap_stays/src/component/HomeNavbar.js
@@ -106,7 +106,7 @@ function HomeNavbar() {
           anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
         >
           <MenuItem component={RouterLink} to ="userprofile">
-          <Avatar />  My Profile
+          <Avatar />  {user.fullName}
         </MenuItem>
           <Divider/>
           <MenuItem onClick={logout} component={RouterLink} to = "/"> Logout 
