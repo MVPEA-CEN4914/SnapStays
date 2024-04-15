2863bd0c527bc10fa2ef337c242e1e42531ef418
EmmanuelM7 <mormani11@gmail.com>
2024-04-15 13:14:24 -0400

    Laying out the information

--- snap_stays/src/pages/IndividualStay.js
+++ snap_stays/src/pages/IndividualStay.js
@@ -2,9 +2,10 @@ import React from 'react'
 import gql from 'graphql-tag';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { useParams } from 'react-router-dom';
-import { Container, Typography, CircularProgress, Grid, Card } from '@mui/material';
+import { Container, Typography, CircularProgress, Grid, Card, Avatar, CardContent, CardMedia, Button, Divider} from '@mui/material';
 import StayCard from "../component/StayCard";
 import TempListing from "../images/TempListing.jpg";
+
 
 function IndividualStay() {
     const{listingid} = useParams();
@@ -18,15 +19,87 @@ function IndividualStay() {
 
   const listing = data.getListing;
 
+  const formatDate = (isoDateString) => {
+    const date = new Date(isoDateString);
+    const options = { month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  };
+
   return (
     <div>
-     <Grid>
-     <Typography variant="h4" textAlign="center" fontFamily="Josefin Sans" fontWeight="600">
+     <Typography variant="h4" fontFamily="Josefin Sans" fontWeight="600">
         <b>{listing.title}</b>
       </Typography>
-     </Grid>
-    
-    
+      <Grid container xs={6} md={12}>
+      <Grid>
+      <Typography variant="h4" fontFamily="Josefin Sans" fontWeight="600">
+        {/* <b>{listing.user.image}</b> */}
+        {listing.price}/month 
+      </Typography>
+      <Typography variant="body2" color="text.secondary">{`${formatDate(
+            listing.leaseStartDate
+          )} - ${formatDate(listing.leaseEndDate)}`}</Typography>
+      {/* <Typography>
+        {listing.leaseStartDate} - {listing.leaseEndDate}
+      </Typography> */}
+      <Typography>
+        {listing.location}
+      </Typography>
+      {/* <Avatar
+                src={listing.user.image}
+                style={{
+                  width: "175px",
+                  height: "175px",
+                  border: "2px solid black",
+                }}
+              /> */}
+      </Grid>
+      <Grid item sm direction="column" alignItems="center" display="flex">
+
+      </Grid>
+      <Grid item xs={12} md={4}>
+      <Avatar variant="rounded"
+                src={listing.images}
+                style={{
+                  width: "175px",
+                  height: "175px",
+                  border: "2px solid black",
+                }}
+      />
+        {/* <img src={listing.images} alt="Description" style={{ width: '90%' }} /> */}
+      </Grid>
+      </Grid>
+      <Card sx={{ maxWidth: 500, mx: 'auto', my: 2 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          {/* <CardMedia
+            component="img"
+            height="140"
+            image={listing.user.image}
+          /> */}
+                <Avatar
+                src={listing.user.image}
+                style={{
+                  width: "150px",
+                  height: "150px",
+                  border: "2px solid black",
+                }}
+              />
+        </Grid>
+        {/* <Divider orientation="vertical" flexItem /> */}
+        <Grid item xs={8}>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Owner: {listing.user.fullName}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Some description given by the user about themselves, it comes from their profile. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Typography>
+            <Button size="small">Message</Button>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </Card>
     </div>
   )
 }
@@ -51,6 +124,7 @@ const GET_LISTING_QUERY = gql`
       user{
         id 
         fullName
+        image
       }
     }
   }
